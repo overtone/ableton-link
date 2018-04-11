e71baf1e6cf79c76a9f7f2f98c6fe61cc5d0fb34
hlolli <hlolli@gmail.com>
2018-04-11 23:34:37 +0200

    docstrings added

--- src/overtone/ableton_link.clj
+++ src/overtone/ableton_link.clj
@@ -3,36 +3,60 @@
 
 (def ableton-link (new AbletonLink))
 
-(defn enable-link [bool]
+(defn enable-link
+  "Enable link"
+  [bool]
   (.enable ableton-link bool))
 
-(defn link-enabled? []
+(defn link-enabled?
+  "Returns true if link is enabled"
+  []
   (.isEnabled ableton-link))
 
-(defn set-bpm [bpm]
+(defn set-bpm
+  "Globally change the bpm on the link"
+  [bpm]
   (.setBpm ableton-link bpm))
 
-(defn get-bpm []
+(defn get-bpm
+  "Get the current global bpm"
+  []
   (.update ableton-link)
   (.getBpm ableton-link))
 
-(defn get-beat []
+(defn get-beat
+  "Sync(update) with link and
+   return the current bpm"
+  []
   (.update ableton-link)
   (.getBeat ableton-link))
 
-(defn set-beat [beat]
+(defn set-beat
+  "Globally set the value of the beat (number)"
+  [beat]
   (.setBeat ableton-link))
 
-(defn set-beat-force [beat]
+(defn set-beat-force
+  "Forcefully and globally set
+   the value of the beat (number)"
+  [beat]
   (.setBeatForce ableton-link))
 
-(defn get-num-peers []
+(defn get-num-peers
+  "Get number of connected peers"
+  []
   (.getNumPeers ableton-link))
 
-(defn set-quantum [quantum]
+(defn set-quantum
+  "Sets the quantum, in a way like setting a
+   time-signature of a bar"
+  [quantum]
   (.setQuantum ableton-link quantum))
 
-(defn get-quantum []
+(defn get-quantum
+  "Get the quantum of a bar, returns number,
+   (return number of beats in a bar)"
+  []
   (.update ableton-link)
   (.getQuantum ableton-link))
 
