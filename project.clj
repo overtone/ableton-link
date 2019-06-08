8ae885e589c5163944523ee4aba4165ad2acb4eb
hlolli <hlolli@gmail.com>
2019-06-08 16:18:22 +0200

    release beta8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject overtone/ableton-link "1.0.0-beta7"
+(defproject overtone/ableton-link "1.0.0-beta8"
   :description "Collaborative Programmable Music."
   :url "http://overtone.github.io/"
   :mailing-list {:name    "overtone"
